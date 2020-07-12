17784aef21c5c89e8800e6553a82d27640e37cee
timesgold <timesgold@kakao.com>
2020-07-12 14:50:29 +0900

    Updates

---
+++ precache-manifest.7a06d483214515f8e53a7f3fa5eb1340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779ce427fde33f3f957b57079b6e36af",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "78332c322a7cc15563fc",
+    "url": "/movie_app_2020/static/css/main.4677db2d.chunk.css"
+  },
+  {
+    "revision": "690baaf60fbc0f30a3d8",
+    "url": "/movie_app_2020/static/js/2.c1169c2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.c1169c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78332c322a7cc15563fc",
+    "url": "/movie_app_2020/static/js/main.db1737b7.chunk.js"
+  },
+  {
+    "revision": "21fda55cf9831c4521c6",
+    "url": "/movie_app_2020/static/js/runtime-main.850efe79.js"
+  }
+]);
